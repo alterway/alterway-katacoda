b0ab0962b0e4486d5d265c73056ae6a35e29a7a0
Herve Leclerc <herve.leclerc@alterway.fr>
2022-02-14 13:56:16 +0100

    task

--- dockercoins-tanka/env-init.sh
+++ dockercoins-tanka/env-init.sh
@@ -23,6 +23,16 @@ echo "Tanka"
 curl -Ls -o "$HOME/go/bin/tk" https://github.com/grafana/tanka/releases/download/v0.10.0/tk-linux-amd64
 chmod a+rx "$HOME/go/bin/tk"
 
+mkdir /tmp/task
+curl -Ls -o "/tmp/task/task_linux_amd64.tar.gz" https://github.com/go-task/task/releases/download/v3.10.0/task_linux_amd64.tar.gz
+cd "/tmp/task" || echo "fail to cd"
+tar -xzf task_linux_amd64.tar.gz
+mv task "$HOME/go/bin/task"
+chmod a+rx "$HOME/go/bin/task"
+rm -rf "/tmp/task"
+
+cd - || echo "fail to cd"
+
 echo "jsonnet"
 pip install jsonnet
 
