f18e8de78443a7d251bf6291cad08e3fb9d08182
Herve Leclerc <herve.leclerc@alterway.fr>
2022-02-14 14:09:00 +0100

    task initilisation

--- dockercoins-tanka/env-init.sh
+++ dockercoins-tanka/env-init.sh
@@ -12,10 +12,6 @@ mkdir -p ~/templates/prod
 
 #echo "Waiting to complete"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Done"
 
-echo "tasks"
-curl -sL https://taskfile.dev/install.sh -o install.sh
-bash install.sh
-
 echo "jb"
 GO111MODULE="on" go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb
 
@@ -23,6 +19,7 @@ echo "Tanka"
 curl -Ls -o "$HOME/go/bin/tk" https://github.com/grafana/tanka/releases/download/v0.10.0/tk-linux-amd64
 chmod a+rx "$HOME/go/bin/tk"
 
+echo "tasks"
 mkdir /tmp/task
 curl -Ls -o "/tmp/task/task_linux_amd64.tar.gz" https://github.com/go-task/task/releases/download/v3.10.0/task_linux_amd64.tar.gz
 cd "/tmp/task" || echo "fail to cd"
